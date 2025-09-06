df9aee264bcbfc7d284f4b252873b6381689e5a8
Frode Solheim <frode@solheim.dev>
2025-09-06 14:45:10 +0200

    Improved scraping

---
+++ replace.sh
@@ -0,0 +1,3 @@
+find scrape -type f -exec sed -i 's|https://wordpress.fs-uae.net|https://fs-uae.net|g' {} +
+find scrape -type f -exec sed -i 's|https:\\/\\/wordpress.fs-uae.net|https:\\/\\/fs-uae.net|g' {} +
+find scrape -type f -exec sed -i 's|https%3A%2F%2Fwordpress.fs-uae.net|https%3A%2F%2Ffs-uae.net|g' {} +
--- scrape.sh
+++ scrape.sh
@@ -4,17 +4,31 @@
 
 mkdir -p scrape
 cd scrape && \
-wget --recursive --no-host-directories \
+wget \
+	--mirror \
+	--no-host-directories \
 	--trust-server-names \
 	--exclude-directories=files,wp-json \
-	https://fs-uae.net/download
+	-e robots=off \
+	https://wordpress.fs-uae.net/
+
+# --convert-links
 
 echo "Scraping done, removing some files..."
 
+echo "Removing some files..."
+
 rm -f "robots.txt"
+rm -rf "wp-admin"
 rm -f "wp-login.php"
 rm -f "wp-login.php?action=lostpassword"
 rm -f "xmlrpc.php?rsd"
+
+echo "Replacing hostname..."
+
+find . -type f -exec sed -i 's|https://wordpress.fs-uae.net|https://fs-uae.net|g' {} +
+find . -type f -exec sed -i 's|https:\\/\\/wordpress.fs-uae.net|https:\\/\\/fs-uae.net|g' {} +
+find . -type f -exec sed -i 's|https%3A%2F%2Fwordpress.fs-uae.net|https%3A%2F%2Ffs-uae.net|g' {} +
 
 echo
 echo "OK."
